015ca2135a81e08f38f77a6165d1071436fcb224
mikesilverii <73197302+mikesilverii@users.noreply.github.com>
2025-04-28 16:35:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ embedding_model = embedding_model = HuggingFaceEmbedding(
     model_name="BAAI/bge-base-en",
     #model_name="NovaSearch/stella_en_1.5B_v5",
     #model_name="michiyasunaga/BioLinkBERT-base",
-    embed_batch_size=32,  # You can tweak this based on your GPU/CPU
+    embed_batch_size=32,  # tweak based on your GPU/CPU
     #query_instruction="Represent this sentence for searching relevant passages:"
     query_instruction="Represent this medical or biomedical query to retrieve related documents."
 )
